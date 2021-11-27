ef436bde75888709240272d5701f5f7d2310db7e
iliapolo <epolon@amazon.com>
2021-11-27 15:10:05 -0800

    remove annotations

--- src/main/java/com/myorg/springbootdeploy/k8s/Manifest.java
+++ src/main/java/com/myorg/springbootdeploy/k8s/Manifest.java
@@ -20,9 +20,7 @@ public class Manifest extends Chart {
                     .build()))
             .build());
 
-    IngressV1Beta1 ingress = deployment.exposeViaIngress(GreetController.PATH);
-    ingress.getMetadata().addAnnotation("kubernetes.io/ingress.class", "alb");
-    ingress.getMetadata().addAnnotation("alb.ingress.kubernetes.io/scheme", "internet-facing");
+    deployment.exposeViaIngress(GreetController.PATH);
   }
 
 }
